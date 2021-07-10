859f6b57c2d7e6a2cfec0b5b78139e6eb7cca7f8
Ashapyapp <87185937+Ashapyapp@users.noreply.github.com>
2021-07-10 13:35:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "6909218"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "bb48b5614bc1aef53622a5f003e0d7ac" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "1870615306:AAFKOUnmgATo5cNHOjM6J03zkeLBD0wTySs" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
